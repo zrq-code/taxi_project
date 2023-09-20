6297830fcd68394e134d61d1c237965868dd58a0
Rick_Zhou <Rick_Zhou@epam.com>
2023-09-20 14:15:52 +0800

    create get verification code api

---
+++ api-passenger/src/main/java/com/mashibing/com/mashibing/request/VerificationCodeDTO.java
@@ -0,0 +1,13 @@
+package com.mashibing.com.mashibing.request;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class VerificationCodeDTO {
+    private String passengerPhone;
+}
---
+++ api-passenger/src/main/java/com/mashibing/controller/VerificationCodeController.java
@@ -0,0 +1,22 @@
+package com.mashibing.controller;
+
+import com.mashibing.com.mashibing.request.VerificationCodeDTO;
+import com.mashibing.service.VerificationCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VerificationCodeController {
+
+    @Autowired
+    private VerificationCodeService verificationCodeService;
+
+    @GetMapping("/verification-code")
+    public String verificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
+        String passengerPhone = verificationCodeDTO.getPassengerPhone();
+        System.out.println("手机号为：" + passengerPhone);
+        return verificationCodeService.generatorCode(passengerPhone);
+    }
+}
