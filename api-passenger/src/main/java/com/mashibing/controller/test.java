86bdc5bb15c893ebba8c87e215139942e9b2c7a5
Rick_Zhou <Rick_Zhou@epam.com>
2023-09-20 11:06:31 +0800

    mashibing taxi project initialization

---
+++ api-passenger/src/main/java/com/mashibing/controller/test.java
@@ -0,0 +1,10 @@
+package com.mashibing.controller;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getProperty("java.compiler"));
+        System.out.println(System.getProperty("java.vm.specification.version"));
+        System.out.println(System.getProperty("java.specification.version"));
+    }
+}
