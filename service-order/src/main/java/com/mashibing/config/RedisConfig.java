9804e2db23da0b4932e35e4d830cf6e4aff5be46
Rick_Zhou <Rick_Zhou@epam.com>
2024-01-03 11:18:41 +0800

    enhance redisConfig

--- service-order/src/main/java/com/mashibing/config/RedisConfig.java
+++ service-order/src/main/java/com/mashibing/config/RedisConfig.java
@@ -3,15 +3,24 @@ package com.mashibing.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RedisConfig {
+
+    private final String potocol = "redis://";
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private String redisPort;
+
     @Bean
-    public RedissonClient redissonClient(){
+    public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:63797").setDatabase(0);
+        config.useSingleServer().setAddress(potocol + redisHost + ":" + redisPort).setDatabase(0);
         return Redisson.create(config);
     }
 }
